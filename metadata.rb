f68f8ba83740ce2c837881529b3e4f80a4929499
davidski <davidski@deadheaven.com>
2014-03-22 06:41:49 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name              'custom'
+maintainer        'Chef Software, Inc.'
+maintainer_email  'cookbooks@opscode.com'
+license           'MIT'
+description       'Configures apt and apt services and LWRPs for managing apt repositories and preferences'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '2.3.9'
+
