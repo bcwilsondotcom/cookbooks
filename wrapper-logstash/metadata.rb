1927947d4bc1bfdbc879f3d55fb6f60c3d6bd930
davidski <davidski@deadheaven.com>
2014-03-24 05:08:30 -0700

    Add logstash wrapper cookbook

---
+++ wrapper-logstash/metadata.rb
@@ -0,0 +1,8 @@
+name             'wrapper-logstash'
+maintainer       'David F. Severski'
+maintainer_email 'davidski@deadheaven.com'
+license          'MIT'
+description      'Installs/Configures wrapper-logstash'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
