bfd5d90b4386a4ccbda2648918b0b73244e98d5e
davidski <davidski@deadheaven.com>
2014-03-30 06:21:49 -0700

    Add server logstash recipe and es-curator

--- wrapper-logstash/recipes/server.rb
+++ wrapper-logstash/recipes/server.rb
@@ -23,19 +23,33 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-include_recipe "logstash"
 
-set['logstash']['instance']['server']['config_templates']   = {
-  'input_syslog' => 'config/input_syslog.conf.erb',
-  'output_stdout' => 'config/output_stdout.conf.erb',
-  'output_elasticsearch' => 'config/output_elasticsearch.conf.erb'
-}
+name = 'server'
+attributes = node['logstash']['instance'][name]
 
-
-begin
-	r = resources(:template => "#{node['logstash']['basedir']}/templates/elk_test.conf.erb")
-	r.cookbooks "wrapper-logstash"
-rescue Chef::Exceptions::ResourceNotFound
-	Chef::Log.warn "Could not find template to override!"
+# these should all default correctly.  listing out for example.
+logstash_instance name do
+  action            :create
 end
+
+# services are hard! Let's go LWRP'ing.   FIREBALL! FIREBALL! FIREBALL!
+logstash_service name do
+  action      [:enable, :start]
+end
+
+es_ip = service_ip(name, 'elasticsearch')
+
+# create our configuration files from the provided templates
+logstash_config name do
+  action [:create]
+  variables(
+      elasticsearch_ip: es_ip,
+      elasticsearch_embedded: true
+  )
+end
+
+logstash_pattern name do
+  action [:create]
+end
+
 
