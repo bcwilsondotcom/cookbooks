05325c5969d114059659735279d787cac63134d9
davidski <davidski@deadheaven.com>
2014-04-06 08:07:23 -0700

    Set logstash version dependecy

--- wrapper-logstash/metadata.rb
+++ wrapper-logstash/metadata.rb
@@ -2,8 +2,8 @@ name             'wrapper-logstash'
 maintainer       'David F. Severski'
 maintainer_email 'davidski@deadheaven.com'
 license          'MIT'
-description      'Installs/Configures wrapper-logstash'
+description      'Installs/Configures logstash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'logstash'
+depends 'logstash', ">= 0.9.0"
