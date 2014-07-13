2f77ec4df25c9f02fbb1773f8b93bb870f32f7d6
davidski <davidski@deadheaven.com>
2014-07-13 07:13:23 -0700

    Update collectd dependcies

--- sch-collectd/metadata.rb
+++ sch-collectd/metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures collectd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'collectd', ">= 1.1.0"
+depends 'collectd', ">= 0.4.2"
