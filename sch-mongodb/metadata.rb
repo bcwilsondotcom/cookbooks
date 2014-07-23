d57105c5eee877ac34efb47d5a9a2a7aef266593
davidski <davidski@deadheaven.com>
2014-07-23 15:50:51 -0700

    Correct cookbook name

--- sch-mongodb/metadata.rb
+++ sch-mongodb/metadata.rb
@@ -1,8 +1,8 @@
-name             'sch_mongodb'
+name             'sch-mongodb'
 maintainer       'David F. Severski'
 maintainer_email 'davidski@deadheaven.com'
 license          'MIT'
-description      'Installs/Configures sch_mongodb'
+description      'Installs/Configures sch-mongodb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
