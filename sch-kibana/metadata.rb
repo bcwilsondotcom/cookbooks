4e955cf4abe1d7799c92ad86f26ae6aa3c780072
davidski <davidski@deadheaven.com>
2014-08-07 11:45:38 -0700

    Support kibana LWRP cookbook

--- sch-kibana/metadata.rb
+++ sch-kibana/metadata.rb
@@ -2,7 +2,8 @@ name             'sch-kibana'
 maintainer       'David F. Severski'
 maintainer_email 'davidski@deadheaven.com'
 license          'MIT'
-description      'Installs/Configures sch-kibana'
-long_description 'Installs/Configures sch-kibana'
+description      'Installs/Configures Kibana'
+long_description 'Installs/Configures Kibana via LWRP'
 version          '0.1.0'
 
+depends 'kibana', ">= 1.3.0"
--- sch-kibana/recipes/default.rb
+++ sch-kibana/recipes/default.rb
@@ -23,3 +23,46 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
+if node['kibana']['user'].empty?
+  if !node['kibana']['webserver'].empty?
+    webserver = node['kibana']['webserver']
+    kibana_user = node[webserver]['user']
+  else
+    kibana_user = 'nobody'
+  end
+else
+  kibana_user = node['kibana']['user']
+  kibana_user kibana_user do
+    name kibana_user
+    group kibana_user
+    home node['kibana']['install_dir']
+    action :create
+  end
+end
+
+kibana_install 'kibana' do
+  user kibana_user
+  group kibana_user
+  install_dir node['kibana']['install_dir']
+  install_type node['kibana']['install_type']
+  action :create
+end
+
+template "#{node['kibana']['install_dir']}/current/config.js" do
+  source node['kibana']['config_template']
+  cookbook node['kibana']['config_cookbook']
+  mode '0750'
+  user kibana_user
+end
+
+link "#{node['kibana']['install_dir']}/current/app/dashboards/default.json" do
+  to 'logstash.json'
+  only_if { !File.symlink?("#{node['kibana']['install_dir']}/current/app/dashboards/default.json") }
+end
+
+kibana_web 'kibana' do
+  type node['kibana']['webserver']
+  docroot "#{node['kibana']['install_dir']}/current"
+  es_server node['kibana']['es_server']
+  not_if { node['kibana']['webserver'].empty? }
+end
